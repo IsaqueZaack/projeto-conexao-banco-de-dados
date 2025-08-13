24b94a75e32a475fce4dce4f7cdab8dbe3f5d4d2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-13 11:29:40 -0300

    Projeto inicial conexao com banco de dados

---
+++ src/main/java/br/fiap/conexao/Conexao.java
@@ -0,0 +1,23 @@
+package br.fiap.conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private static String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
+    private static String USER = "rm558864";
+    private static String PASSWORD = "020306";
+
+    private Conexao() {}
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        catch (SQLException e) {
+            System.out.println("Erro ao conectar no banco de dados\n" + e);
+        }
+        return null;
+    }
+}
